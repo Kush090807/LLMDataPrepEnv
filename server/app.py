a5226437c5bcd735b30108cc70e3cd9529c5705b
Kush090807 <kushpatel9a7@gmail.com>
2026-04-08 12:37:43 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -9,6 +9,13 @@ ActionType = Union[UpdateValueAction, DeleteRowAction, PassAction]
 
 
 app = create_fastapi_app(LLMDataPrepEnv, ActionType, Observation)
+def main():
+    import uvicorn
+   
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+
+if __name__ == "__main__":
+    main()
 
 if __name__ == "__main__":
     import uvicorn
