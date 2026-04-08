fedaa6aadc27693969be3c388c650e53481ce480
Kush090807 <kushpatel9a7@gmail.com>
2026-04-08 12:32:16 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -4,15 +4,11 @@ from openenv.core.env_server import create_fastapi_app
 from env import LLMDataPrepEnv
 from models import UpdateValueAction, DeleteRowAction, PassAction, Observation
 
-# 1. Define your actions (Must match your models.py)
+
 ActionType = Union[UpdateValueAction, DeleteRowAction, PassAction]
 
-# 2. Initialize your environment class
-env = LLMDataPrepEnv()
 
-# 3. Create the FastAPI app using the OpenEnv helper
-# This is the "magic" that makes your code a web server
-app = create_fastapi_app(env, ActionType, Observation)
+app = create_fastapi_app(LLMDataPrepEnv, ActionType, Observation)
 
 if __name__ == "__main__":
     import uvicorn
